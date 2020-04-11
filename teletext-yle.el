5580de1412349ee579cc07329476fded848b6384
Lassi Kortela <lassi@lassi.io>
2020-04-11 11:17:15 +0300

    Add more detail to commentary

--- teletext-yle.el
+++ teletext-yle.el
@@ -14,7 +14,11 @@
 ;; Provides a teletext broadcast from YLE, the Finnish national
 ;; television network.  The service is free of charge and a TV tuner
 ;; is not needed for browsing: an ordinary internet connection is
-;; enough.  The broadcast is retrieved from YLE's official public API.
+;; enough.  You don't need a personal YLE account to use this.
+;;
+;; The broadcast is retrieved from YLE's official public API.  This
+;; Emacs package is not made by YLE and is not endorsed by them or
+;; affiliated with them in any way.
 ;;
 ;; Note: The vast majority of YLE teletext pages are in Finnish with a
 ;; small selection in Swedish and only news headlines in English.
